ec0d61efad83d58a7f4baeaed51e976ab481df53
Eugine1234 <omudekeugine5@gmail.com>
2023-09-07 10:13:14 +0300

    first to commit
    first  date:  06/08/2023

---
+++ programing2.py
@@ -0,0 +1,52 @@
+ input ('&&->')
+if bus=='1':
+   print('There is a giant like man here eating mandazi and tea.')
+   print('What do you do?')
+   print('[1]Take the mandazi and tea.')
+   print('[2]scream at the man.\n')
+
+   man= input('&&->')
+
+   if man=='1':
+     print('The man blows your ass off, good job but soo bad!!')
+     man=='2'print("You travel to Nairobi by two buses,Do you go by bus#1 or by bus#2?\n") 
+bus=
+
+print('The man breaks your arm off, good job but also so bad')
+
+print('well, doing{man}is actually better,')
+print('The man runs away...')
+
+bus=='2'
+print('You stair into the endless abyss at cthulhu\n retina.')
+print('[1]blue berries.')
+print('[2]yellow jacket clothespins.')
+print('[3]understanding revolvers,yelling melodies.\n')
+ 
+ #print('press q to go back and choose a different bus\n')
+
+insanity=input('&&->')
+if insanity=='1' or insanity == '2':
+    print('your body survives powered by a mind of jello.')
+    print('good job!')
+
+else:
+    print('The insanity rots your eyes into a pool of muck.')
+    print('good job!')
+
+
+
+print('you stumble around and fall on a knife and you pass away. Is it really good you pass away?')
+again:input('yes/no:&&->')
+ 
+if'no'or 'n':
+ 
+ print('dont\worry, you will make good decissions in real life.')
+again=='yes' or again=='y'
+print('seems like you dont want to be in this world any more.')
+
+
+print('INVALID RESPONSE!!!')
+
+
+     #SystemError()
